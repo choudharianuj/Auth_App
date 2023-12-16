122eff4fbaf84c4043f3a956b6a9aae10f9088f1
ianujchoudhari <choudharianuj42@gmail.com>
2023-12-16 15:18:12 +0530

    all the pages of the site are created without any content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import{Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/signin' element={<SignIn />}/>
+      <Route path='/signup' element={<SignUp />}/>
+      <Route path='/profile' element={<Profile />}/>
+    </Routes>
   )
 }
 
